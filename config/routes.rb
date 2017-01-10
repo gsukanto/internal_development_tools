5c8cc95ab5fc9bae8e26fdf3ca7ecb09e6ddc99f
developer satu <devsatu@dev1.local>
2017-01-10 16:58:25 +0700

    remove PUT, DESTROY, etc for card infos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :card_infos
+  get 'card_infos' => 'card_infos#index'
   get 'show' => 'card_infos#show'
   get 'cvv' => 'card_infos#cvv'
   root 'card_infos#index'
